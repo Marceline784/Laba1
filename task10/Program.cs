e520c0f48ade30bf227d60ba34282a48063dba40
Marceline <mariailnytska2@gmail.com>
2025-09-06 20:28:22 +0300

    Completed task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -3,7 +3,10 @@ internal class Program
 {
     static void Main()
     {
-       
+        Console.WriteLine("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
+        bool result = n % 9 == 0 || n % 11 == 0 || n % 13 == 0;
+        Console.WriteLine(result);
     }
 }
 
