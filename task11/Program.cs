680359450786f5ee6c20d397adead121a0d5bb84
Marceline <mariailnytska2@gmail.com>
2025-09-06 20:40:32 +0300

    Completed task11

--- task11/Program.cs
+++ task11/Program.cs
@@ -3,7 +3,26 @@ internal class Program
 {
     static void Main()
     {
-        
+        Console.WriteLine("Enter a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter b: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter c: ");
+        int c = int.Parse(Console.ReadLine());
+        int biggest;
+        if (a > b && a > c)
+        {
+            biggest = a;
+        }
+        else if (b > a && b > c)
+        {
+            biggest = b;
+        }
+        else
+        {
+            biggest = c;
+        }
+        Console.WriteLine($"Result = {biggest}");
     }
 }
 
