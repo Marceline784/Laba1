cc59216339869c08fc780da3bf91910e2976397c
Marceline <mariailnytska2@gmail.com>
2025-09-06 19:46:57 +0300

    Completed task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -3,7 +3,11 @@ internal class Program
 {
     static void Main()
     {
+        Console.WriteLine("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
 
+        int lastDigit = n % 10;
+        Console.WriteLine($"Last digit = {lastDigit}");
     }
 }
 
