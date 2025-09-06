fe73f0e84893cce2d35f72ce276e0c6762dc0a0d
Marceline <mariailnytska2@gmail.com>
2025-09-06 19:12:34 +0300

    Completed task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -3,7 +3,16 @@ internal class Program
 {
     static void Main()
     {
-        
+        Console.WriteLine("Enter a: ");
+        float a = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter b: ");
+        float b = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter h: ");
+        float h = float.Parse(Console.ReadLine());
+        Console.WriteLine($"a = {a}, b = {b}, h = {h}");
+        float area = ((a + b) / 2f) * h;
+        Console.WriteLine($"Average = {area}");
+
     }
 }
 
